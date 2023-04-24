a29178a79c3906b95737e5ef9c2c7228ab23e65a
Sophia Tony-Egbuniwe <ost2105@columbia.edu>
2023-04-24 15:10:05 -0500

    Feature 9 complete

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from accounts.forms import LoginForm
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 
 # Create your views here.
 
@@ -25,3 +25,8 @@ def user_login(request):
         "form": form,
     }
     return render(request, "accounts/login.html", context)
+
+
+def user_logout(request):
+    logout(request)
+    return redirect("login")
