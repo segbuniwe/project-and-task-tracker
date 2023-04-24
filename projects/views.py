ddc78c80466501f0ea61b6a71b1e8f0e58788443
Sophia Tony-Egbuniwe <ost2105@columbia.edu>
2023-04-24 16:03:38 -0500

    Feature 13 complete

--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from projects.models import Project
 from django.contrib.auth.decorators import login_required
 
@@ -12,3 +12,12 @@ def list_projects(request):
         "project_list": project_list,
     }
     return render(request, "projects/list.html", context)
+
+
+@login_required
+def show_project(request, id):
+    project_detail = get_object_or_404(Project, id=id)
+    context = {
+        "project_detail": project_detail,
+    }
+    return render(request, "projects/detail.html", context)
