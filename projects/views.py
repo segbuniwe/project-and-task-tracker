ef26c34b6ee5ea016fcbf0d1dfba0949bafe170d
Sophia Tony-Egbuniwe <ost2105@columbia.edu>
2023-04-24 16:22:43 -0500

    Feature 14 complete

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from projects.models import Project
 from django.contrib.auth.decorators import login_required
+from projects.forms import ProjectForm
 
 # Create your views here.
 
@@ -21,3 +22,20 @@ def show_project(request, id):
         "project_detail": project_detail,
     }
     return render(request, "projects/detail.html", context)
+
+
+@login_required
+def create_project(request):
+    if request.method == "POST":
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            project = form.save(False)
+            project.owner = request.user
+            form.save()
+            return redirect("list_projects")
+    else:
+        form = ProjectForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "projects/create_project.html", context)
